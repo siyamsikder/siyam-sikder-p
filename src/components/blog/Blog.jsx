68161e2b83109527b9d2a2e1f110e24848cac62b
Siyam Sikder <siyam0sikder@gmail.com>
2026-01-13 19:33:08 +0600

    Limit home blog posts to 3 and add View All button

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -1,15 +1,24 @@
 import SectionTitle from '../common/SectionTitle';
 import BlogCard from './BlogCard';
 import { blogs } from '../../data/blogs';
+import Button from '../common/Button';
+import { NavLink } from 'react-router-dom';
 
 const Blog = () => {
+    const latestBlogs = blogs.slice(0, 3);
+
     return (
         <section id="blog" className="py-20">
             <SectionTitle title="Latest Articles" subtitle="Blog" />
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-10">
-                {blogs.map((blog, index) => (
+                {latestBlogs.map((blog, index) => (
                     <BlogCard key={blog.id} blog={blog} index={index} />
                 ))}
+            </div>
+            <div className="mt-12 text-center">
+                <NavLink to="/blog">
+                    <Button variant="outline">View All Articles</Button>
+                </NavLink>
             </div>
         </section>
     );
